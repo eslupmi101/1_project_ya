e01ec286abde78b5b6ec5377ec7001bdb1a306e4
eslupmi101 <ajsen-andreev@yandex.ru>
2023-01-13 17:53:09 +0900

    Update function 'main'

--- character_creation_module/main.py
+++ character_creation_module/main.py
@@ -105,7 +105,7 @@ def choice_char_class():
     return char_class
 
 
-def main():
+if __name__ == '__main__':
     """Основной сценарий игры."""
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
@@ -116,6 +116,3 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
